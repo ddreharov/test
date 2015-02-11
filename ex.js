a13f163de593b86de92ddbc12173818d821d6455
dimitar <d.dreharov@gmail.com>
2015-02-11 21:11:47 +0200

    implementing ex1

--- ex.js
+++ ex.js
@@ -1,5 +1,15 @@
 function fizzFuz() {
     for (i=0; i<100; i++) {
-        console.log(i);
+        if(i%3) {
+            console.log("fizz");
+        }
+        if(i%5) {
+            console.log('buzz');
+        }
+        if(i%3 && i%5 ) {
+            console.log('Fizzbuz')
+        }
     }
 }
+
+fizzFuz();
